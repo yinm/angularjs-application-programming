273a1761a6874323ee472584cc5fdfb9b9821f11
yinm <yinm.cminor9@gmail.com>
2017-12-16 15:00:09 +0900

    Check iframe with sce

--- chap03/resource/scripts/iframe_trust.js
+++ chap03/resource/scripts/iframe_trust.js
@@ -1,5 +1,5 @@
+// 1st
 angular.module('myApp', [])
   .controller('MyController', ['$scope', '$sce', function($scope, $sce) {
     $scope.url = $sce.trustAs($sce.RESOURCE_URL, 'http://www.wings.msn.to/');
-    // $scope.url = $sce.trustAsResourceUrl('http://www.wings.msn.to/');
   }]);
