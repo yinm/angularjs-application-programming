02fa1851e5f0d8b94f79139ea1ca95a605aaf297
yinm <yinm.cminor9@gmail.com>
2017-12-16 14:42:32 +0900

    Check href 2

--- chap03/resource/scripts/href.js
+++ chap03/resource/scripts/href.js
@@ -1,17 +1,6 @@
-/*
 angular.module('myApp', [])
-  .controller('MyController', ['$scope', function($scope) {
-    $scope.url = 'https://gihyo.jp/dp';
-  }]);
-*/
-// angular.module('myApp', [])
-//   .controller('MyController', ['$scope', '$timeout',  function($scope, $timeout) {
-//     $timeout(function() {
-//       $scope.url = 'https://gihyo.jp/dp';
-//     }, 3000);
-//   }]);
-
-angular.module('myApp', [])
-  .controller('MyController', ['$scope', function($scope) {
-    $scope.url = 'https://gihyo.jp/dp';
+  .controller('MyController', ['$scope', '$timeout', function($scope, $timeout) {
+    $timeout(function() {
+      $scope.url = 'https://gihyo.jp/dp';
+    }, 3000);
   }]);
