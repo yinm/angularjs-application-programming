3ee63894c95094238dda7034f9dbe01b74f06ba1
yinm <yinm.cminor9@gmail.com>
2017-12-16 14:35:49 +0900

    Check plurize

--- chap03/bind/scripts/plurize.js
+++ chap03/bind/scripts/plurize.js
@@ -1,6 +1,5 @@
+// 1st
 angular.module('myApp', [])
-//angular.module('myApp', [ 'ngMessageFormat' ])
   .controller('MyController', ['$scope', function($scope) {
-    $scope.favs = [ '山田理央', '鈴木洋平', '腰掛奈美', '田中哲市', '佐藤令' ];
+    $scope.favs = [ '山田理央', '鈴木洋平' ];
   }]);
-
