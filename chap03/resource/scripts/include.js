58eab92b3f18d1852d75b5605d8a45d460c55f59
yinm <yinm.cminor9@gmail.com>
2017-12-16 15:21:09 +0900

    Check ng-include

--- chap03/resource/scripts/include.js
+++ chap03/resource/scripts/include.js
@@ -1,14 +1,6 @@
+// 1st
 angular.module('myApp', [])
   .controller('MyController', ['$scope', function($scope) {
-
-// 第6章 インクルード時に発生する$emitイベント
-/*
-    $scope.$on('$includeContentRequested', function(e, path) {
-      console.log(e);
-      console.log(path);
-    });
-*/
-
     $scope.templates = [
       { title: 'execution', url: 'templates/execution.html' },
       { title: 'tempo', url: 'templates/tempo.html' }
