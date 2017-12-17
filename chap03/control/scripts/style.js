ca35e3d1813f7a37617056be89d103a7956aacdb
yinm <yinm.cminor9@gmail.com>
2017-12-17 15:14:06 +0900

    Check ng-style

--- chap03/control/scripts/style.js
+++ chap03/control/scripts/style.js
@@ -1,8 +1,4 @@
+// 1st
 angular.module('myApp', [])
-  .controller('MyController', ['$scope', function($scope) {
-    $scope.author = {
-      name: 'YAMADA, Yoshihiro',
-      gender: 'male',
-      birth: new Date()
-    };
+  .controller('MyController' ['$scope', function($scope) {
   }]);
