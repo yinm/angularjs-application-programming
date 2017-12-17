472a3ac71680fbcfe1d8901207939eb615b235c6
yinm <yinm.cminor9@gmail.com>
2017-12-17 15:05:13 +0900

    Check $event.stopPropagation method

--- chap03/event/scripts/event_bubble.js
+++ chap03/event/scripts/event_bubble.js
@@ -1,10 +1,11 @@
+// 1st
 angular.module('myApp', [])
   .controller('MyController', ['$scope', function($scope) {
     $scope.onclick1 = function() {
-      console.log("outerをクリックしました！");
+      console.log('outerをクリックしました！');
     };
     $scope.onclick2 = function($event) {
       $event.stopPropagation();
-      console.log("innerをクリックしました！");
+      console.log('innerをクリックしました！');
     };
   }]);
