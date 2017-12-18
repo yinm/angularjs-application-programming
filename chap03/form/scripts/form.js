03a62b36d1bcda27c8bcb99eb9125ca6164d0879
yinm <yinm.cminor9@gmail.com>
2017-12-18 09:55:14 +0900

    Check form validation

--- chap03/form/scripts/form.js
+++ chap03/form/scripts/form.js
@@ -1,9 +1,10 @@
+// 1st
 angular.module('myApp', [])
   .controller('MyController', ['$scope', function($scope) {
     $scope.onsubmit = function() {
-      console.log('メールアドレス：' + $scope.user.mail);
-      console.log('パスワード：' + $scope.user.passwd);
-      console.log('名前（漢字）：' + $scope.user.name);
-      console.log('備考：' + $scope.user.memo);
+      console.log('メールアドレス:' + $scope.user.mail);
+      console.log('パスワード:' + $scope.user.passwd);
+      console.log('名前(漢字):' + $scope.user.name);
+      console.log('備考:' + $scope.user.memo);
     };
   }]);
