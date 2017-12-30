42f223ab23902c877b54d6059b387a1d2856ad9b
yinm <yinm.cminor9@gmail.com>
2017-12-30 12:58:26 +0900

    Check $httpProvider.defaults.transformRequest

--- chap05/http/scripts/http_request.js
+++ chap05/http/scripts/http_request.js
@@ -1,29 +1,28 @@
 angular.module('myApp', [])
-  .config(['$httpProvider', function($httpProvider) {
+  .config(['$httpProvider', ($httpProvider) => {
     $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
-    $httpProvider.defaults.transformRequest.push(function(data) {
+    $httpProvider.defaults.transformRequest.push((data) => {
       data = JSON.parse(data);
-      var query = [];
-      for(var key in data) {
-        query.push(encodeURIComponent(key) + "="
-          + encodeURIComponent(data[key]));
-      };
-      return query.join("&");
+      let query = [];
+
+      for (let key in data) {
+        query.push(encodeURIComponent(key) + '=' + encodeURIComponent(data[key]));
+      }
+      return query.join('&');
     })
   }])
 
-
-  .controller('MyController', ['$scope', '$http', function($scope, $http) {
-    $scope.onclick = function() {
+  .controller('MyController', ['$scope', '$http', ($scope, $http) => {
+    $scope.onclick = () => {
       $http({
         method: 'POST',
         url: 'http_request.php',
-        data: { name: $scope.name }
+        data: { name: $scope.name },
       })
-      .success(function(data, status, headers, config){
+      .success((data, status, headers, config) => {
         $scope.result = data;
       })
-      .error(function(data, status, headers, config){
+      .error((data, status, headers, config) => {
         $scope.result = '!!通信に失敗しました!!';
       });
     };
