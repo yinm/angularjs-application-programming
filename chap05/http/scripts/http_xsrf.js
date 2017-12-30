afd8f14a18ab18f95520bb2ec255f6762fa47943
yinm <yinm.cminor9@gmail.com>
2017-12-30 13:29:40 +0900

    Check $httpProvider.defaults.xsrf*

--- chap05/http/scripts/http_xsrf.js
+++ chap05/http/scripts/http_xsrf.js
@@ -1,15 +1,22 @@
 angular.module('myApp', [])
-  .config(['$httpProvider', function($httpProvider) {
+  .config(['$httpProvider', ($httpProvider) => {
     $httpProvider.defaults.xsrfCookieName = 'CSRF-TOKEN';
     $httpProvider.defaults.xsrfHeaderName = 'X-CSRF-TOKEN';
   }])
-  .controller('MyController', ['$scope', '$http', function($scope, $http) {
-    $scope.onclick = function() {
-      $http.get('http.php', { params: { name: $scope.name } })
-      .success(function(data, status, headers, config){
+  .controller('MyController', ['$scope', '$http', ($scope, $http) => {
+    $scope.onclick = () => {
+      $http.get(
+        'http.php',
+        {
+          params: {
+            name: $scope.name,
+          },
+        },
+      )
+      .success((data, status, headers, config) => {
         $scope.result = data;
       })
-      .error(function(data, status, headers, config){
+      .error((data, status, headers, config) => {
         $scope.result = '!!通信に失敗しました!!';
       });
     };
