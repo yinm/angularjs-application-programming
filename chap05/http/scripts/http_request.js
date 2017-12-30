af6cc8a8317bca41bdbb66d9a2afcdabd70ad4f6
yinm <yinm.cminor9@gmail.com>
2017-12-30 13:13:39 +0900

    Check $httpParamSerializerJQLike

--- chap05/http/scripts/http_request.js
+++ chap05/http/scripts/http_request.js
@@ -1,53 +1,23 @@
 angular.module('myApp', [])
-  .config(['$httpProvider', ($httpProvider) => {
-    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
-    $httpProvider.defaults.transformRequest.push((data) => {
-      data = JSON.parse(data);
-      let query = [];
-
-      for (let key in data) {
-        query.push(encodeURIComponent(key) + '=' + encodeURIComponent(data[key]));
-      }
-      return query.join('&');
-    })
-  }])
-
-  .controller('MyController', ['$scope', '$http', ($scope, $http) => {
-    $scope.onclick = () => {
-      $http({
-        method: 'POST',
-        url: 'http_request.php',
-        data: { name: $scope.name },
-      })
-      .success((data, status, headers, config) => {
-        $scope.result = data;
-      })
-      .error((data, status, headers, config) => {
-        $scope.result = '!!通信に失敗しました!!';
-      });
-    };
-  }]);
-
-/*
-  .controller('MyController',
+  .controller(
+    'MyController',
     ['$scope', '$http', '$httpParamSerializerJQLike',
-    function($scope, $http, $httpParamSerializerJQLike) {
-    $scope.onclick = function() {
-      $http({
-        method: 'POST',
-        headers: {
-          'Content-Type' : 'application/x-www-form-urlencoded;charset=utf-8'
-        },
-        transformRequest: $httpParamSerializerJQLike,
-        url: 'http_request.php',
-        data: { name: $scope.name }
-      })
-      .success(function(data, status, headers, config){
-        $scope.result = data;
-      })
-      .error(function(data, status, headers, config){
-        $scope.result = '!!通信に失敗しました!!';
-      });
-    };
-  }]);
-*/
+    ($scope, $http, $httpParamSerializerJQLike) => {
+      $scope.onclick = () => {
+        $http({
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+          },
+          transformRequest: $httpParamSerializerJQLike,
+          url: 'http_request.php',
+          data: { name: $scope.name },
+        })
+        .success((data, status, headers, config) => {
+          $scope.result = data;
+        })
+        .error((data, status, headers, config) => {
+          $scope.result = '!!通信に失敗しました!!';
+        });
+      };
+    }]);
