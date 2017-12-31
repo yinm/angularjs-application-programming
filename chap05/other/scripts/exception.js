5216d46353f404d84fee39dfcc6622233e16ca0f
yinm <yinm.cminor9@gmail.com>
2017-12-31 16:44:44 +0900

    Check $exceptionHandler

--- chap05/other/scripts/exception.js
+++ chap05/other/scripts/exception.js
@@ -1,13 +1,13 @@
 angular.module('myApp', [])
-  .factory('$exceptionHandler'
-  , ['$window', '$log', function ($window, $log) {
-    return function (exception, cause) {
-      $window.alert('ページで不明なエラーが発生しました。\r'
-        + '時間をおいてから、再度アクセスしてください。');
+  .factory('$exceptionHandler', ['$window', '$log', ($window, $log) => {
+    return (exception, cause) => {
+      $window.alert(
+        'ページで不明なエラーが発生しました。\r' +
+        '時間をおいてから、再度アクセスしてください。'
+      );
       $log.error(exception);
-      // $log.error(exception.message);
-    };
+    }
   }])
-  .controller('MyController', ['$scope', function($scope){
-    throw new Error('エラーが発生しました！');
+  .controller('MyController', ['$scope', ($scope) => {
+    throw new Error('エラーが発生しました。');
   }]);
