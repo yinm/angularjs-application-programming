9a72e626ee8f428b93eba2b8860bee01351762d5
yinm <yinm.cminor9@gmail.com>
2018-01-01 16:52:42 +0900

    Check $scope.$apply

--- chap06/scripts/apply.js
+++ chap06/scripts/apply.js
@@ -2,32 +2,15 @@ angular.module('myApp', [])
   .controller('MyController', ['$scope', '$window', ($scope, $window) => {
     $window.navigator.geolocation.watchPosition(
       (pos) => {
-        $scope.latitude = pos.coords.latitude;
-        $scope.longitude = pos.coords.longitude;
+        if (!$scope.$$phase) {
+          $scope.$apply((scope) => {
+            scope.latitude = pos.coords.latitude;
+            scope.longitude = pos.coords.longitude;
+          });
+        }
       },
       (e) => {
         $window.alert(e.message);
       }
     );
   }]);
-
-// angular.module('myApp', [])
-//   .controller('MyController',
-//   ['$scope', '$window', function($scope, $window) {
-//     $window.navigator.geolocation.watchPosition(
-//       function(pos) {
-//         // $scope.latitude = pos.coords.latitude;
-//         // $scope.longitude = pos.coords.longitude;
-//
-//         if (!$scope.$$phase) {
-//           $scope.$apply(function(scope) {
-//             scope.latitude = pos.coords.latitude;
-//             scope.longitude = pos.coords.longitude;
-//           });
-//         }
-//       },
-//       function(e) {
-//         $window.alert(e.message);
-//       }
-//     );
-//   }]);
