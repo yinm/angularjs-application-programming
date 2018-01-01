e5f56440dd09205aa37cfc8191023d3b6c71c7e0
yinm <yinm.cminor9@gmail.com>
2018-01-01 16:18:51 +0900

    Check $scope.$emit, $scope.$broadcast, $scope.on

--- chap06/scripts/event.js
+++ chap06/scripts/event.js
@@ -1,28 +1,25 @@
 angular.module('myApp', [])
-  .controller('MyController', ['$scope', function($scope) {
-    $scope.onemit = function() {
+  .controller('MyController', ['$scope', ($scope) => {
+    $scope.onemit = () => {
       $scope.$emit('notify', 'Emit');
     };
 
-    $scope.onbroad = function() {
+    $scope.onbroad = () => {
       $scope.$broadcast('notify', 'Broadcast', new Date());
     };
   }])
-  .controller('ParentController', ['$scope', function($scope) {
-    $scope.$on('notify', function(e, data) {
-      $scope.parentResult = e.name + '/' + e.targetScope.message
-        + '/' + data;
+  .controller('ParentController', ['$scope', ($scope) => {
+    $scope.$on('notify', (e, data) => {
+      $scope.parentResult = `${e.name}/${e.targetScope.message}/${data}`;
     });
   }])
-  .controller('Child1Controller', ['$scope', function($scope) {
-    $scope.$on('notify', function(e, data, current) {
-      $scope.child1Result = e.name + '/' + e.targetScope.message
-        + '/' + data + '/' + current.toLocaleString();
+  .controller('Child1Controller', ['$scope', ($scope) => {
+    $scope.$on('notify', (e, data, current) => {
+      $scope.child1Result = `${e.name}/${e.targetScope.message}/${data}/${current.toLocaleString()}`;
     });
   }])
-  .controller('Child2Controller', ['$scope', function($scope) {
-    $scope.$on('notify', function(e, data, current) {
-      $scope.child2Result = e.name + '/' + e.targetScope.message
-        + '/' + data + '/' + current.toLocaleString();
+  .controller('Child2Controller', ['$scope', ($scope) => {
+    $scope.$on('notify', (e, data, current) => {
+      $scope.child2Result = `${e.name}/${e.targetScope.message}/${data}/${current.toLocaleString()}`;
     });
   }]);
