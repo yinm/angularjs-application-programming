7fb3c4b350946fd2ab1efc3cd5d2c3e3efe25e7e
yinm <yinm.cminor9@gmail.com>
2018-01-01 19:11:33 +0900

    Check $scope.$watchGroup 2

--- chap06/scripts/watch_group.js
+++ chap06/scripts/watch_group.js
@@ -5,41 +5,17 @@ angular.module('myApp', [])
     $scope.area = 0;
 
     $scope.cancel =
-      $scope.$watchGroup(['base', 'height'], (newValue, oldValue, scope) => {
-        scope.area = scope.base * scope.height / 2;
-      });
+      $scope.$watchGroup(
+        [
+          () => $scope.base,
+          () => $scope.height
+        ],
+        (newValue, oldValue, scope) => {
+          scope.area = scope.base * scope.height / 2;
+        }
+      );
 
     $scope.onstop = () => {
       $scope.cancel();
-    }
+    };
   }]);
-
-// angular.module('myApp', [])
-//   .controller('MyController', ['$scope', function($scope) {
-//     $scope.base = 0;
-//     $scope.height = 0;
-//     $scope.area = 0;
-//
-// /*
-//     $scope.cancel =
-//       $scope.$watchGroup(['base', 'height'],
-//         function(newValue, oldValue, scope) {
-//         scope.area = scope.base * scope.height / 2;
-//       });
-// */
-//
-//     $scope.cancel =
-//       $scope.$watchGroup(
-//         [
-//           function() { return $scope.base; },
-//           function() { return $scope.height; }
-//         ],
-//         function(newValue, oldValue, scope) {
-//         scope.area = scope.base * scope.height / 2;
-//       });
-//
-//     $scope.onstop = function() {
-//       $scope.cancel();
-//     };
-//
-//   }]);
