2d4959433e1f41478d25cfd8237d204b379349c6
yinm <yinm.cminor9@gmail.com>
2018-01-01 15:45:05 +0900

    Check nested scope 2

--- chap06/scripts/nest_proto.js
+++ chap06/scripts/nest_proto.js
@@ -1,16 +1,14 @@
 angular.module('myApp', [])
-  .controller('ParentController', ['$scope', function($scope) {
+  .controller('ParentController', ['$scope', ($scope) => {
     $scope.value = 1;
 
-    $scope.onparent = function() {
+    $scope.onparent = () => {
       $scope.value++;
     };
-
   }])
-  .controller('ChildController', ['$scope', function($scope) {
-    $scope.onchild = function() {
+  .controller('ChildController', ['$scope', ($scope) => {
+    $scope.onchild = () => {
       $scope.value++;
       //$scope.$parent.value++;
-    };
+    }
   }]);
-
