6e2a030f105c222db9c27309105ecb62d7aa4417
yinm <yinm.cminor9@gmail.com>
2018-01-01 19:08:49 +0900

    Check $scope.$watchGroup

--- chap06/scripts/watch_group.js
+++ chap06/scripts/watch_group.js
@@ -1,29 +1,45 @@
 angular.module('myApp', [])
-  .controller('MyController', ['$scope', function($scope) {
+  .controller('MyController', ['$scope', ($scope) => {
     $scope.base = 0;
     $scope.height = 0;
     $scope.area = 0;
 
-/*
     $scope.cancel =
-      $scope.$watchGroup(['base', 'height'],
-        function(newValue, oldValue, scope) {
-        scope.area = scope.base * scope.height / 2;
-      });
-*/
-
-    $scope.cancel =
-      $scope.$watchGroup(
-        [
-          function() { return $scope.base; },
-          function() { return $scope.height; }
-        ],
-        function(newValue, oldValue, scope) {
+      $scope.$watchGroup(['base', 'height'], (newValue, oldValue, scope) => {
         scope.area = scope.base * scope.height / 2;
       });
 
-    $scope.onstop = function() {
+    $scope.onstop = () => {
       $scope.cancel();
-    };
-
+    }
   }]);
+
+// angular.module('myApp', [])
+//   .controller('MyController', ['$scope', function($scope) {
+//     $scope.base = 0;
+//     $scope.height = 0;
+//     $scope.area = 0;
+//
+// /*
+//     $scope.cancel =
+//       $scope.$watchGroup(['base', 'height'],
+//         function(newValue, oldValue, scope) {
+//         scope.area = scope.base * scope.height / 2;
+//       });
+// */
+//
+//     $scope.cancel =
+//       $scope.$watchGroup(
+//         [
+//           function() { return $scope.base; },
+//           function() { return $scope.height; }
+//         ],
+//         function(newValue, oldValue, scope) {
+//         scope.area = scope.base * scope.height / 2;
+//       });
+//
+//     $scope.onstop = function() {
+//       $scope.cancel();
+//     };
+//
+//   }]);
