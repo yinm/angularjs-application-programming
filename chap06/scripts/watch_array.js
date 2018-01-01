66b9c5d0e65e6acb3c87855d921743b8e9d72418
yinm <yinm.cminor9@gmail.com>
2018-01-01 19:19:44 +0900

    Check $scope.$watch equality

--- chap06/scripts/watch_array.js
+++ chap06/scripts/watch_array.js
@@ -1,9 +1,9 @@
 angular.module('myApp', [])
-  .controller('MyController', ['$scope', function($scope) {
+  .controller('MyController', ['$scope', ($scope) => {
     $scope.triangle = { base: 0, height: 0 };
     $scope.area = 0;
 
-    $scope.$watch('triangle', function(newValue, oldValue, scope) {
+    $scope.$watch('triangle', (newValue, oldValue, scope) => {
       scope.area = newValue.base * newValue.height / 2;
     }, true);
   }]);
