4f5eae71a9eb53dc23afae61a99e5f205f5eae2e
yinm <yinm.cminor9@gmail.com>
2018-01-02 08:46:42 +0900

    Check filter with arguments

--- chap07/filter/scripts/param.js
+++ chap07/filter/scripts/param.js
@@ -1,12 +1,14 @@
 angular.module('myApp', [])
-  .filter('truncate', function (){
-    return function(value, length, omission) {
+  .filter('truncate', () => {
+    return (value, length, omission) => {
       if (!angular.isString(value)) {
         return value;
       }
+
       if (!angular.isNumber(length)) {
         length = 50;
-      };
+      }
+
       omission = omission || '...';
       if (value.length <= length) {
         return value;
