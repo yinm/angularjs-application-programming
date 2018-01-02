d055337adef4cbe828a976bd984a17220613a491
yinm <yinm.cminor9@gmail.com>
2018-01-02 17:40:41 +0900

    Check scope property

--- chap07/directive/scripts/scope.js
+++ chap07/directive/scripts/scope.js
@@ -1,16 +1,16 @@
 angular.module('myApp', [])
-  .directive('myBook', function() {
+  .directive('myBook', () => {
     return {
       restrict: 'E',
       replace: true,
       scope: false,
-      template: '<div>{{book.title}}（{{book.price | number}}円／{{book.publish}}）</div>'
+      template: '<div>{{book.title}} ({{book.price | number}}円/{{book.publish}})</div>',
     }
   })
-  .controller('MyController', ['$scope', function($scope) {
+  .controller('MyController', ['$scope', ($scope) => {
     $scope.book = {
       title: 'JavaScript本格入門',
       price: 2980,
-      publish: '技術評論社'
+      publish: '技術評論社',
     };
   }]);
