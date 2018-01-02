f4e201803036730b8cc4a2f3c19511aa3929f7b3
yinm <yinm.cminor9@gmail.com>
2018-01-02 12:19:40 +0900

    Check singleton

--- chap07/service/scripts/singleton.js
+++ chap07/service/scripts/singleton.js
@@ -1,58 +1,58 @@
-/*
 angular.module('myApp', [])
   .service('SharedService', function() {
     this.name = '権兵衛';
-    this.getMessage = function() {
-      return 'こんにちは、' + this.name + 'さん！';
+    this.getMessage = () => {
+      return `こんにちは、${this.name}さん！`;
     }
   })
   .controller('PrevController', ['$scope', 'SharedService',
-    function($scope, SharedService) {
-    var svc = SharedService;
-    $scope.shared = svc;
-    $scope.name = svc.name;
-    $scope.onclick = function() {
-      svc.name = $scope.name;
-    };
-  }])
-  .controller('NextController', ['$scope', 'SharedService',
-    function($scope, SharedService) {
-    var svc = SharedService;
-    $scope.shared = svc;
-    $scope.name = svc.name;
-    $scope.onclick = function() {
-      svc.name = $scope.name;
-    };
-  }]);
-*/
+    ($scope, SharedService) => {
+      const svc = SharedService;
 
-angular.module('myApp', [])
-  .service('SharedService', function() {
-    var MyService = function() {
-      this.name = '権兵衛';
-      this.getMessage = function() {
-        return 'こんにちは、' + this.name + 'さん！';
+      $scope.shared = svc;
+      $scope.name = svc.name;
+      $scope.onclick = () => {
+        svc.name = $scope.name;
       };
-    };
-    return function() {
-      return new MyService();
-    };
-  })
-  .controller('PrevController', ['$scope', 'SharedService',
-    function($scope, SharedService) {
-    var svc = SharedService();
-    $scope.shared = svc;
-    $scope.name = svc.name;
-    $scope.onclick = function() {
-      svc.name = $scope.name;
-    };
   }])
   .controller('NextController', ['$scope', 'SharedService',
-    function($scope, SharedService) {
-    var svc = SharedService();
-    $scope.shared = svc;
-    $scope.name = svc.name;
-    $scope.onclick = function() {
-      svc.name = $scope.name;
-    };
+    ($scope, SharedService) => {
+      const svc = SharedService;
+
+      $scope.shared = svc;
+      $scope.name = svc.name;
+      $scope.onclick = () => {
+        svc.name = $scope.name;
+      }
   }]);
+
+// angular.module('myApp', [])
+//   .service('SharedService', function() {
+//     var MyService = function() {
+//       this.name = '権兵衛';
+//       this.getMessage = function() {
+//         return 'こんにちは、' + this.name + 'さん！';
+//       };
+//     };
+//     return function() {
+//       return new MyService();
+//     };
+//   })
+//   .controller('PrevController', ['$scope', 'SharedService',
+//     function($scope, SharedService) {
+//     var svc = SharedService();
+//     $scope.shared = svc;
+//     $scope.name = svc.name;
+//     $scope.onclick = function() {
+//       svc.name = $scope.name;
+//     };
+//   }])
+//   .controller('NextController', ['$scope', 'SharedService',
+//     function($scope, SharedService) {
+//     var svc = SharedService();
+//     $scope.shared = svc;
+//     $scope.name = svc.name;
+//     $scope.onclick = function() {
+//       svc.name = $scope.name;
+//     };
+//   }]);
