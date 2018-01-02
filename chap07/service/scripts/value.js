b34329ad268b990dfed231825b9cebbe70db9d3e
yinm <yinm.cminor9@gmail.com>
2018-01-02 09:26:33 +0900

    Check value()

--- chap07/service/scripts/value.js
+++ chap07/service/scripts/value.js
@@ -1,24 +1,40 @@
 angular.module('myApp', [])
-/*
-  .config(['AppTitle', 'AppInfo', 'CommonProc',
-    function(AppTitle, AppInfo, CommonProc) {
-    console.log(AppTitle);
-    console.log(AppInfo);
-    CommonProc(AppInfo.author);
-  }])
-*/
   .value('AppTitle', 'AngularJSプログラミング')
   .value('AppInfo', {
     author: '山田理央',
-    updated: new Date(2007, 5, 25)
+    updated: new Date(2007, 5, 25),
   })
-  .value('CommonProc', function(value) {
+  .value('CommonProc', (value) => {
     console.log(value);
   })
-
   .controller('MyController', ['$scope', 'AppTitle', 'AppInfo', 'CommonProc',
-    function($scope, AppTitle, AppInfo, CommonProc) {
-    $scope.title = AppTitle;
-    $scope.info = AppInfo;
-    $scope.proc = CommonProc;
-  }]);
+    ($scope, AppTitle, AppInfo, CommonProc) => {
+      $scope.title = AppTitle;
+      $scope.info = AppInfo;
+      $scope.proc = CommonProc;
+    }]);
+
+// angular.module('myApp', [])
+// /*
+//   .config(['AppTitle', 'AppInfo', 'CommonProc',
+//     function(AppTitle, AppInfo, CommonProc) {
+//     console.log(AppTitle);
+//     console.log(AppInfo);
+//     CommonProc(AppInfo.author);
+//   }])
+// */
+//   .value('AppTitle', 'AngularJSプログラミング')
+//   .value('AppInfo', {
+//     author: '山田理央',
+//     updated: new Date(2007, 5, 25)
+//   })
+//   .value('CommonProc', function(value) {
+//     console.log(value);
+//   })
+//
+//   .controller('MyController', ['$scope', 'AppTitle', 'AppInfo', 'CommonProc',
+//     function($scope, AppTitle, AppInfo, CommonProc) {
+//     $scope.title = AppTitle;
+//     $scope.info = AppInfo;
+//     $scope.proc = CommonProc;
+//   }]);
