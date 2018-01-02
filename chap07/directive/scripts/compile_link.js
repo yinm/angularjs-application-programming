c27803c964c52254ce4aff8fc0073cb308e70897
yinm <yinm.cminor9@gmail.com>
2018-01-02 19:00:39 +0900

    Check compile and link

--- chap07/directive/scripts/compile_link.js
+++ chap07/directive/scripts/compile_link.js
@@ -1,21 +1,20 @@
 angular.module('myApp', [])
-  .directive('compileLink', function() {
+  .directive('compileLink', () => {
     return {
       restrict: 'E',
-      //terminal: true,
-      compile: function(element, attrs) {
-        console.log('compile: ' + attrs.type);
+      compile: (element, attrs) => {
+        console.log(`compile: ${attrs.type}`);
         return {
-          pre: function(scope, element, attrs, controller) {
-            console.log('prelink: ' + attrs.type);
+          pre: (scope, element, attrs, controller) => {
+            console.log(`prelink: ${attrs.type}`);
           },
-          post: function(scope, element, attrs, controller) {
-            console.log('postlink: ' + attrs.type);
+          post: (scope, element, attrs, controller) => {
+            console.log(`postlink: ${attrs.type}`);
           }
         };
       },
     };
   })
-  .controller('MyController', ['$scope', function($scope) {
+  .controller('MyController', ['$scope', ($scope) => {
     $scope.data = ['い', 'ろ', 'は', 'に', 'ほ'];
   }]);
