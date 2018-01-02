507f9caa8ee0c065695fcc50910d160052c80ffe
yinm <yinm.cminor9@gmail.com>
2018-01-02 11:49:33 +0900

    Check $provide service

--- chap07/service/scripts/value_provide.js
+++ chap07/service/scripts/value_provide.js
@@ -1,18 +1,17 @@
 angular.module('myApp', [])
-  .config(['$provide', function($provide) {
+  .config(['$provide', ($provide) => {
     $provide.value('AppTitle', 'AngularJSプログラミング');
     $provide.value('AppInfo', {
       author: '山田理央',
-      updated: new Date(2007, 5, 25)
+      updated: new Date(2007, 5, 25),
     });
-    $provide.value('CommonProc', function(value) {
+    $provide.value('CommonProc', (value) => {
       console.log(value);
     })
   }])
-
   .controller('MyController', ['$scope', 'AppTitle', 'AppInfo', 'CommonProc',
-    function($scope, AppTitle, AppInfo, CommonProc) {
-    $scope.title = AppTitle;
-    $scope.info = AppInfo;
-    $scope.proc = CommonProc;
+    ($scope, AppTitle, AppInfo, CommonProc) => {
+      $scope.title = AppTitle;
+      $scope.info = AppInfo;
+      $scope.proc = CommonProc;
   }]);
