e05837f3852e7c12e44c52dc55731dde90319bda
yinm <yinm.cminor9@gmail.com>
2018-01-02 18:01:23 +0900

    Check isolated scope property

--- chap07/directive/scripts/scope_isolate.js
+++ chap07/directive/scripts/scope_isolate.js
@@ -1,18 +1,18 @@
 angular.module('myApp', [])
-  .directive('myBook', function() {
+  .directive('myBook', () => {
     return {
       restrict: 'E',
       replace: true,
       scope: {
-        book: '=myInfo'
+        book: '=myInfo',
       },
-      template: '<div>{{book.title}}（{{book.price | number}}円／{{book.publish}}）</div>'
+      template: '<div>{{book.title}} ({{book.price | number}}円/{{book.publish}})</div>',
     }
   })
-  .controller('MyController', ['$scope', function($scope) {
+  .controller('MyController', ['$scope', ($scope) => {
     $scope.data = {
       title: 'JavaScript本格入門',
       price: 2980,
-      publish: '技術評論社'
+      publish: '技術評論社',
     };
   }]);
