12f6dda08c61ec31237c59a8e7eb27527885544f
yinm <yinm.cminor9@gmail.com>
2018-01-02 09:33:55 +0900

    Check constant

--- chap07/service/scripts/constant.js
+++ chap07/service/scripts/constant.js
@@ -1,24 +1,40 @@
 angular.module('myApp', [])
-/*
-  .config(['AppTitle', 'AppInfo', 'CommonProc',
-    function(AppTitle, AppInfo, CommonProc) {
-    console.log(AppTitle);
-    console.log(AppInfo);
-    CommonProc(AppInfo.author);
-  }])
-*/
   .constant('AppTitle', 'AngularJSプログラミング')
   .constant('AppInfo', {
     author: '山田理央',
-    updated: new Date(2007, 5, 25)
+    updated: new Date(2007, 5, 25),
   })
-  .constant('CommonProc', function(constant) {
+  .constant('CommonProc', (constant) => {
     console.log(constant);
   })
-
   .controller('MyController', ['$scope', 'AppTitle', 'AppInfo', 'CommonProc',
-    function($scope, AppTitle, AppInfo, CommonProc) {
-    $scope.title = AppTitle;
-    $scope.info = AppInfo;
-    $scope.proc = CommonProc;
-  }]);
+    ($scope, AppTitle, AppInfo, CommonProc) => {
+      $scope.title = AppTitle;
+      $scope.info = AppInfo;
+      $scope.proc = CommonProc;
+    }]);
+
+// angular.module('myApp', [])
+// /*
+//   .config(['AppTitle', 'AppInfo', 'CommonProc',
+//     function(AppTitle, AppInfo, CommonProc) {
+//     console.log(AppTitle);
+//     console.log(AppInfo);
+//     CommonProc(AppInfo.author);
+//   }])
+// */
+//   .constant('AppTitle', 'AngularJSプログラミング')
+//   .constant('AppInfo', {
+//     author: '山田理央',
+//     updated: new Date(2007, 5, 25)
+//   })
+//   .constant('CommonProc', function(constant) {
+//     console.log(constant);
+//   })
+//
+//   .controller('MyController', ['$scope', 'AppTitle', 'AppInfo', 'CommonProc',
+//     function($scope, AppTitle, AppInfo, CommonProc) {
+//     $scope.title = AppTitle;
+//     $scope.info = AppInfo;
+//     $scope.proc = CommonProc;
+//   }]);
