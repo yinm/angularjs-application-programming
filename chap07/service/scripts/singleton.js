8bc46e689b9dd3b97eb49ef9af98b56e8e8df760
yinm <yinm.cminor9@gmail.com>
2018-01-02 12:35:53 +0900

    Check singleton 2

--- chap07/service/scripts/singleton.js
+++ chap07/service/scripts/singleton.js
@@ -1,13 +1,18 @@
 angular.module('myApp', [])
-  .service('SharedService', function() {
-    this.name = '権兵衛';
-    this.getMessage = () => {
-      return `こんにちは、${this.name}さん！`;
+  .service('SharedService', () => {
+    const MyService = function() {
+      this.name = '権兵衛';
+      this.getMessage = () => {
+        return `こんにちは、${this.name}さん！`;
+      };
+    };
+    return () => {
+      return new MyService();
     }
   })
   .controller('PrevController', ['$scope', 'SharedService',
     ($scope, SharedService) => {
-      const svc = SharedService;
+      const svc = SharedService();
 
       $scope.shared = svc;
       $scope.name = svc.name;
@@ -17,42 +22,11 @@ angular.module('myApp', [])
   }])
   .controller('NextController', ['$scope', 'SharedService',
     ($scope, SharedService) => {
-      const svc = SharedService;
+      const svc = SharedService();
 
       $scope.shared = svc;
       $scope.name = svc.name;
       $scope.onclick = () => {
         svc.name = $scope.name;
-      }
+      };
   }]);
-
-// angular.module('myApp', [])
-//   .service('SharedService', function() {
-//     var MyService = function() {
-//       this.name = '権兵衛';
-//       this.getMessage = function() {
-//         return 'こんにちは、' + this.name + 'さん！';
-//       };
-//     };
-//     return function() {
-//       return new MyService();
-//     };
-//   })
-//   .controller('PrevController', ['$scope', 'SharedService',
-//     function($scope, SharedService) {
-//     var svc = SharedService();
-//     $scope.shared = svc;
-//     $scope.name = svc.name;
-//     $scope.onclick = function() {
-//       svc.name = $scope.name;
-//     };
-//   }])
-//   .controller('NextController', ['$scope', 'SharedService',
-//     function($scope, SharedService) {
-//     var svc = SharedService();
-//     $scope.shared = svc;
-//     $scope.name = svc.name;
-//     $scope.onclick = function() {
-//       svc.name = $scope.name;
-//     };
-//   }]);
