d193bf1f05410bac9b16c2e3f377569aaa3da626
yinm <yinm.cminor9@gmail.com>
2018-01-05 08:32:46 +0900

    Check directive test

--- chap08/UnitTest/scripts/directive.js
+++ chap08/UnitTest/scripts/directive.js
@@ -1,19 +1,18 @@
 angular.module('myApp.directive', [])
-  .directive('myBook', function() {
+  .directive('myBook', () => {
     return {
       restrict: 'E',
       replace: true,
       scope: {
         book: '=myBook'
       },
-      template: '<div>{{book.title}}（{{book.price | number}}円／{{book.publish}}）</div>'
-    }
+      template: '<div>{{book.title}}（{{book.price | number}}円／{{book.publish}}）</div>',
+    };
   })
-  .controller('myController', ['$scope', function($scope) {
+  .controller('myController', ['$scope', ($scope) => {
     $scope.data = {
       title: 'JavaScript本格入門',
       price: 2980,
       publish: '技術評論社',
-
     };
   }]);
