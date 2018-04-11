e12ad825a3c29940785c8f9b990a37cd980b56f9
yinm <yinm.cminor9@gmail.com>
2018-04-11 09:11:21 +0900

    Check shortcut method

--- chap05/http/scripts/http.js
+++ chap05/http/scripts/http.js
@@ -1,16 +1,12 @@
 angular.module('myApp', [])
   .controller('MyController', ['$scope', '$http', ($scope, $http) => {
     $scope.onclick = () => {
-      $http({
-        method: 'GET',
-        url: 'http.php',
-        params: { name: $scope.name }
-      })
+      $http.get('http.php', {params: {name: $scope.name}})
         .success((data, status, headers, config) => {
           $scope.result = data
         })
         .error((data, status, headers, config) => {
-          $scope.result = '!!通信に失敗しました!!'
+          $scpoe.result = '通信失敗'
         })
     }
   }])
