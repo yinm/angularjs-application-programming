2b7de6a1e2dd3c835f55fa21111b18eef21dc31e
yinm <yinm.cminor9@gmail.com>
2018-04-11 21:19:28 +0900

    Check transformRequest property

--- chap05/http/scripts/http_request.js
+++ chap05/http/scripts/http_request.js
@@ -1,23 +1,29 @@
 angular.module('myApp', [])
-  .controller(
-    'MyController',
-    ['$scope', '$http', '$httpParamSerializerJQLike',
-    ($scope, $http, $httpParamSerializerJQLike) => {
-      $scope.onclick = () => {
-        $http({
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
-          },
-          transformRequest: $httpParamSerializerJQLike,
-          url: 'http_request.php',
-          data: { name: $scope.name },
-        })
+  .config(['$httpProvider', ($httpProvider) => {
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8'
+    $httpProvider.defaults.transformRequest.push((data) => {
+      data = JSON.parse(data)
+      let query = []
+
+      for (let key in data) {
+        query.push(encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+      }
+
+      return query.join('&')
+    })
+  }])
+  .controller('MyController', ['$scope', '$http', ($scope, $http) => {
+    $scope.onclick = () => {
+      $http({
+        method: 'POST',
+        url: 'http_request.php',
+        data: { name: $scope.name },
+      })
         .success((data, status, headers, config) => {
-          $scope.result = data;
+          $scope.result = data
         })
         .error((data, status, headers, config) => {
-          $scope.result = '!!通信に失敗しました!!';
-        });
-      };
-    }]);
+          $scope.result = '通信失敗'
+        })
+    }
+  }])
