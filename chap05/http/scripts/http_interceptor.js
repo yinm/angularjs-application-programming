49fc022ae48287b0e9cc0d9c79a2e81795cbf4a1
yinm <yinm.cminor9@gmail.com>
2018-04-11 23:12:17 +0900

    Check $httpProvider.interceptors

--- chap05/http/scripts/http_interceptor.js
+++ chap05/http/scripts/http_interceptor.js
@@ -1,52 +1,53 @@
 angular.module('myApp', [])
-  .config(['$httpProvider', function($httpProvider) {
+  .config(['$httpProvider', ($httpProvider) => {
     $httpProvider.interceptors.push(
-      [ '$q', '$log', '$window', function ($q, $log, $window) {
+      ['$q', '$log', '$window', ($q, $log, $window) => {
         return {
-          'request': function(config) {
-            config.startTime = (new Date()).getTime();
-            $log.info('request...');
-            $log.info(config);
-            return config;
+          request(config) {
+            config.startTime = (new Date()).getTime()
+            $log.info('request...')
+            $log.info(config)
+            return config
           },
-          'requestError': function(rejection) {
-            $log.info('requestError...');
-            $log.info(rejection);
-            return $q.reject(rejection);
+          requestError(rejection) {
+            $log.info('requestError...')
+            $log.info(rejection)
+            return $q.reject(rejection)
           },
-          'response': function(response) {
-            response.config.endTime = (new Date()).getTime();
-            $log.info('Process Time(sec): ' + (response.config.endTime - response.config.startTime) / 1000);
+          response(response) {
+            response.config.endTime = (new Date()).getTime()
+            $log.info('Process Time(sec): ' + (response.config.endTime - response.config.startTime) / 1000)
 
-            $log.info('response...');
-            $log.info(response);
-            return response;
+            $log.info('response...')
+            $log.info(response)
+            return response
           },
-          'responseError': function(rejection) {
+          responseError(rejection) {
             if (rejection.status === 500) {
-              $window.alert('$http service failed !');
-              location.href = 'top.html';
+              $window.alert('$http service failed!')
+              location.href = 'top.html'
             }
-            $log.info('responseError...');
-            $log.info(rejection);
-            return $q.reject(rejection);
-          }
-        };
-      }]);
+            $log.info('responseError...')
+            $log.info(rejection)
+            return $q.reject(rejection)
+          },
+        }
+      }]
+    )
   }])
-
-  .controller('MyController', ['$scope', '$http', function($scope, $http) {
-    $scope.onclick = function() {
+  .controller('MyController', ['$scope', '$http', ($scope, $http) => {
+    $scope.onclick = () => {
       $http({
         method: 'POST',
         url: 'http_interceptor.php',
-        data: { name: $scope.name }
+        data: { name: $scope.name },
       })
-      .success(function(data, status, headers, config){
-        $scope.result = data;
-      })
-      .error(function(data, status, headers, config){
-        $scope.result = '!!通信に失敗しました!!';
-      });
-    };
-  }]);
+        .success((data, status, headers, config) => {
+          $scope.result = data
+        })
+        .error((data, status, headers, config) => {
+          $scope.result = '通信失敗'
+        })
+    }
+  }])
+
