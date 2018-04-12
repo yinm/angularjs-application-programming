312aeeee57c5ffa288adb166dd67a18a7b8953f9
yinm <yinm.cminor9@gmail.com>
2018-04-13 08:56:55 +0900

    Check $resource

--- chap05/resource/scripts/resource.js
+++ chap05/resource/scripts/resource.js
@@ -1,40 +1,42 @@
-angular.module('myApp', [ 'ngResource' ])
+angular.module('myApp', ['ngResource'])
   .controller('MyController',
-   ['$scope', '$resource', function($scope, $resource) {
-    var Book = $resource(
-      'resource.php/:isbn',
-      { isbn: '@isbn' },
-      //{ isbn: '@isbn', id: 108, charset: 'utf8' },
-      { update: { method: 'PUT' } }
-    );
+    ['$scope', '$resource', ($scope, $resource) => {
+      const Book = $resource(
+        'resource.php/:isbn',
+        { isbn: '@isbn' },
+        { update: { method: 'PUT' } }
+      )
 
-    $scope.books = Book.query();
+      $scope.books = Book.query()
 
-    $scope.oninsert = function() {
-      Book.save(
-        $scope.book,
-        function() {
-          $scope.books = Book.query();
-        });
-    };
+      $scope.oninsert = () => {
+        Book.save(
+          $scope.book,
+          () => {
+            $scope.books = Book.query()
+          }
+        )
+      }
 
-    $scope.onedit = function(isbn) {
-      $scope.book = Book.get({ isbn: isbn });
-    };
+      $scope.onedit = (isbn) => {
+        $scope.book = Book.get({ isbn: isbn })
+      }
 
-    $scope.onupdate = function() {
-      Book.update(
-        $scope.book,
-        function() {
-          $scope.books = Book.query();
-        });
-    };
+      $scope.onupdate = () => {
+        Book.update(
+          $scope.book,
+          () => {
+            $scope.books = Book.query()
+          }
+        )
+      }
 
-    $scope.ondelete = function(isbn) {
-      Book.delete(
-        { isbn: isbn },
-        function() {
-          $scope.books = Book.query();
-        });
-    };
-  }]);
+      $scope.ondelete = (isbn) => {
+        Book.delete(
+          { isbn: isbn },
+          () => {
+            $scope.books = Book.query()
+          }
+        )
+      }
+    }])
