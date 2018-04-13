ac486064c0f216d7e0ea8160a6a295a11059d138
yinm <yinm.cminor9@gmail.com>
2018-04-13 09:39:20 +0900

    Check instance method

--- chap05/resource/scripts/resource_instance.js
+++ chap05/resource/scripts/resource_instance.js
@@ -1,63 +1,64 @@
-angular.module('myApp', [ 'ngResource' ])
+angular.module('myApp', ['ngResource'])
   .controller('MyController',
-   ['$scope', '$resource', function($scope, $resource) {
+    ['$scope', '$resource', ($scope, $resource) => {
+      const Book = $resource(
+        'resource.php/:isbn',
+        { isbn: '@isbn' },
+        { update: { method: 'PUT' } }
+      )
+      $scope.books = Book.query()
 
-    var Book = $resource(
-      'resource.php/:isbn',
-      { isbn: '@isbn' },
-      { update: { method: 'PUT' } }
-    );
-    $scope.books = Book.query();
+      let b = Book.get({ isbn: '978-4-7741-7078-7' }, () => {
+        b.title = 'サーバサイドJavaポケットリファレンス'
+        b.$update(() => {
+          $scope.books = Book.query()
+        })
+      })
 
-/*    var b = Book.get({ isbn: '978-4-7741-7078-7' }, function() {
-      b.title = 'サーバサイドJavaポケットリファレンス';
-      b.$update(function() {
-        $scope.books = Book.query();
-      });
-    });
-*/
+      Book.get({ isbn: '978-4-7741-7078-7' }, (b) => {
+        b.title = 'サーバサイドJavaポケットリファレンス'
+        b.$update(() => {
+          $scope.books = Book.query()
+        })
+      })
 
-    Book.get({ isbn: '978-4-7741-7078-7' }, function(b) {
-      b.title = 'サーバサイドJavaポケットリファレンス';
-      b.$update(function() {
-        $scope.books = Book.query();
-      });
-    });
+      boook = new Book({ isbn: '978-4-7741-9999-0' })
+      boook.title = 'AngularJSポケットリファレンス'
+      boook.price = 3000
+      boook.publish = '技術評論社'
+      boook.published = '2015-10-10'
+      boook.$save(() => {
+        $scope.books = Book.query()
+      })
 
-    var b = new Book({ isbn: '978-4-7741-XXXX-X' });
-    b.title = 'AngularJSポケットリファレンス';
-    b.price = 3000;
-    b.publish = '技術評論社';
-    b.published = '2015-10-10';
-    b.$save(function() {
-      $scope.books = Book.query();
-    });
+      $scope.oninsert = () => {
+        Book.save(
+          $scope.book,
+          () => {
+            $scope.books = Book.query()
+          }
+        )
+      }
 
-    $scope.oninsert = function() {
-      Book.save(
-        $scope.book,
-        function() {
-          $scope.books = Book.query();
-        });
-    };
+      $scope.onedit = (isbn) => {
+        $scope.book = Book.get({ isbn: isbn })
+      }
 
-    $scope.onedit = function(isbn) {
-      $scope.book = Book.get({ isbn: isbn });
-    };
+      $scope.onupdate = () => {
+        Book.update(
+          $scope.book,
+          () => {
+            $scope.books = Book.query()
+          }
+        )
+      }
 
-    $scope.onupdate = function() {
-      Book.update(
-        $scope.book,
-        function() {
-          $scope.books = Book.query();
-        });
-    };
-
-    $scope.ondelete = function(isbn) {
-      Book.delete(
-        { isbn: isbn },
-        function() {
-          $scope.books = Book.query();
-        });
-    };
-  }]);
+      $scope.ondelete = (isbn) => {
+        Book.delete(
+          { isbn: isbn },
+          () => {
+            $scope.books = Book.query()
+          }
+        )
+      }
+  }])
