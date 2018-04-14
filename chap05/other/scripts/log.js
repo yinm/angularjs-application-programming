2a836d5c464cb3ca7c993b6ed2addbc7efb19148
yinm <yinm.cminor9@gmail.com>
2018-04-14 16:50:08 +0900

    Check $log service

--- chap05/other/scripts/log.js
+++ chap05/other/scripts/log.js
@@ -1,11 +1,11 @@
 angular.module('myApp', [])
-  .config(['$logProvider', function($logProvider) {
-    $logProvider.debugEnabled(true);
+  .config(['$logProvider', ($logProvider) => {
+    $logProvider.debugEnabled(true)
   }])
-  .controller('MyController', ['$scope', '$log',  function($scope, $log) {
-    $log.debug('デバッグ');
-    $log.error('エラー');
-    $log.info('情報');
-    $log.log('一般ログ');
-    $log.warn('警告');
-  }]);
+  .controller('MyController', ['$scope', '$log', ($scope, $log) => {
+    $log.debug('デバッグ')
+    $log.error('エラー')
+    $log.info('情報')
+    $log.log('一般ログ')
+    $log.warn('警告')
+  }])
