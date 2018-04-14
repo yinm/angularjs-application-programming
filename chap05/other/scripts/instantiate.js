1c30acec85ac00343272eba15c3a2a0c62f9a5e8
yinm <yinm.cminor9@gmail.com>
2018-04-14 17:20:43 +0900

    Check instantiate

--- chap05/other/scripts/instantiate.js
+++ chap05/other/scripts/instantiate.js
@@ -1,16 +1,17 @@
 angular.module('myApp', [])
-  .value('birth', new Date(2007, 5, 25));
+  .value('birth', new Date(2007, 5, 25))
 
-var Animal = function (name, birth, $log) {
-  this.name = name;
-  this.birth = birth;
-  this.output = function() {
-    $log.debug(name + ':' + birth.toLocaleString());
-  };
-};
+const Animal = (name, birth, $log) => {
+  this.name = name
+  this.birth = birth
+  this.output = () => {
+    $log.info(`${name}:${birth.toLocalString()}`)
+  }
+}
 
-var $injector = angular.injector(['ng', 'myApp']);
-var ani = $injector.instantiate(
-  ['name', 'birth', '$log', Animal], { name : 'ウタ' });
+const $injector = angular.injector(['ng', 'myApp'])
+const ani = $injector.instance(
+  ['name', 'birth', '$log', Animal], { name: 'ウタ' }
+)
 
-ani.output();
+ani.output()
