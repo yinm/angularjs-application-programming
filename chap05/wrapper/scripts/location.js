9598ce48df56afa636890f696bbcd5d9bc996784
yinm <yinm.cminor9@gmail.com>
2018-04-14 10:31:42 +0900

    Check $location service

--- chap05/wrapper/scripts/location.js
+++ chap05/wrapper/scripts/location.js
@@ -1,20 +1,18 @@
 angular.module('myApp', [])
-/*
-  .config(function($locationProvider){
-     $locationProvider.hashPrefix('!');
-  })
-*/
+  /*
+    .config(function($locationProvider){
+       $locationProvider.hashPrefix('!');
+    })
+  */
 
-  .controller('MyController',
-    ['$scope', '$location', function($scope, $location) {
-    $scope.onclick = function() {
-      $location.url('articles?id=108#wings');
-    };
-
-/*
-    if ($location.path() === '/articles') {
-      console.log('id値：' + $location.search().id);
+  .controller('MyController', ['$scope', '$location', ($scope, $location) => {
+    $scope.onClick = () => {
+      $location.url('articles?id=108#wings')
     }
-*/
-  }]);
 
+    /*
+        if ($location.path() === '/articles') {
+          console.log('id値：' + $location.search().id);
+        }
+    */
+  }])
