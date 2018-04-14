c08aa5510adeedbaa5ad3fa37557b0a4a1d13689
yinm <yinm.cminor9@gmail.com>
2018-04-14 16:09:18 +0900

    Check Promise.all

--- chap05/promise/scripts/promise_all.js
+++ chap05/promise/scripts/promise_all.js
@@ -1,30 +1,33 @@
 angular.module('myApp', [])
-  .controller('MyController', ['$scope', '$timeout', '$log', '$q', function($scope, $timeout, $log, $q) {
-    var asyncProcess = function(value) {
-      var deferred = $q.defer();
+  .controller('MyController', ['$scope', '$timeout', '$log', '$q', ($scope, $timeout, $log, $q) => {
+    const asyncProcess = (value) => {
+      const deferred = $q.defer()
+
       $timeout(
-        function() {
-          deferred.notify('asyncProcess');
+        () => {
+          deferred.notify('asyncProcess')
           if (value === undefined || value === '') {
-            deferred.reject('入力値が空です');
+            deferred.reject('入力値が空です')
           } else {
-            deferred.resolve('入力値は' + value);
+            deferred.resolve(`入力値は${value}`)
           }
-        }, 1000);
-      return deferred.promise;
-    };
+        }
+      , 1000)
+
+      return deferred.promise
+    }
 
     $q.all([
       asyncProcess('トクジロウ'),
       asyncProcess('ニンザブロウ'),
       asyncProcess('リンリン')
     ])
-    .then(
-      function(o_resolve) {
-        console.log(o_resolve);
-      },
-      function(o_reject) {
-        console.log(o_reject);
-      }
-    );
-  }]);
+      .then(
+        (resolve) => {
+           console.log(resolve)
+        },
+        (reject) => {
+          console.log(reject)
+        }
+      )
+  }])
