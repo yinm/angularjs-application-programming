724611dce826daad8ed214f7839752384f52e6fb
yinm <yinm.cminor9@gmail.com>
2018-04-14 13:46:02 +0900

    Check Promise

--- chap05/promise/scripts/promise.js
+++ chap05/promise/scripts/promise.js
@@ -1,42 +1,73 @@
+// angular.module('myApp', [])
+//   .controller('MyController', ['$scope', '$timeout', '$log', '$q', function($scope, $timeout, $log, $q) {
+//     var asyncProcess = function(value) {
+//       var deferred = $q.defer();
+//       $timeout(
+//         function() {
+//           deferred.notify('asyncProcess');
+//           if (value === undefined || value === '') {
+//             deferred.reject('入力値が空です');
+//           } else {
+//             deferred.resolve('入力値は' + value);
+//           }
+//         }, 1000);
+//       return deferred.promise;
+//     };
+//
+//     asyncProcess('トクジロウ')
+//     // asyncProcess()
+//       .then(
+//         function(o_resolve) {
+//           $log.info(o_resolve);
+//           //return '**' + o_resolve + '**';
+//         },
+//         function(o_reject) {
+//           $log.info(o_reject);
+//           // return $q.reject('**' + o_reject + '**');
+//         },
+//         function(o_notify) {
+//           $log.info(o_notify);
+//         }
+//       );
+//       /*
+//       .then(
+//         function(o_resolve) {
+//           $log.info(o_resolve);
+//         },
+//         function(o_reject) {
+//           $log.info(o_reject);
+//         }
+//       );
+//       */
+//   }]);
+
 angular.module('myApp', [])
-  .controller('MyController', ['$scope', '$timeout', '$log', '$q', function($scope, $timeout, $log, $q) {
-    var asyncProcess = function(value) {
-      var deferred = $q.defer();
+  .controller('MyController', ['$scope', '$timeout', '$log', '$q', ($scope, $timeout, $log, $q) => {
+    const asyncProcess = (value) => {
+      const deferred = $q.defer()
       $timeout(
-        function() {
-          deferred.notify('asyncProcess');
+        () => {
+          deferred.notify('asyncProcess')
           if (value === undefined || value === '') {
-            deferred.reject('入力値が空です');
+            deferred.reject('入力値が空です')
           } else {
-            deferred.resolve('入力値は' + value);
+            deferred.resolve(`入力値は${value}`)
           }
-        }, 1000);
-      return deferred.promise;
-    };
+        }, 1000)
+
+      return deferred.promise
+    }
 
     asyncProcess('トクジロウ')
-    // asyncProcess()
       .then(
-        function(o_resolve) {
-          $log.info(o_resolve);
-          //return '**' + o_resolve + '**';
-        }, 
-        function(o_reject) {
-          $log.info(o_reject);
-          // return $q.reject('**' + o_reject + '**');
+        (resolve) => {
+          $log.info(resolve)
         },
-        function(o_notify) {
-          $log.info(o_notify);
-        }
-      );
-      /*
-      .then(
-        function(o_resolve) {
-          $log.info(o_resolve);
+        (reject) => {
+          $log.info(reject)
         },
-        function(o_reject) {
-          $log.info(o_reject);
+        (notify) => {
+          $log.info(notify)
         }
-      );
-      */
-  }]);
+      )
+  }])
