f0e90e4a108bc11ce0c7ea32d8b4af5eb9488fc9
yinm <yinm.cminor9@gmail.com>
2018-04-14 15:25:23 +0900

    Add stub for exec

--- chap05/promise/scripts/geo.js
+++ chap05/promise/scripts/geo.js
@@ -11,26 +11,48 @@ angular.module('myApp', [])
             return deferred.reject(err)
           }
         )
+        deferred.resolve('a')
 
         return deferred.promise
       }
 
       const getSunset = (coords) => {
-        const today = new Date()
-        return $http.jsonp('http://www.finds.jp/ws/movesun.php',
-          {
-            params: {
-              jsonp: 'JSON_CALLBACK',
-              lat: coords.latitude,
-              lon: coords.longitude,
-              y: today.getFullYear(),
-              m: today.getMonth() + 1,
-              d: today.getDate(),
-              tz: 9.0,
-              e: 0
+        const deferred = $q.defer()
+        const stub = {
+          data: {
+            result: {
+              event: [
+                {
+                  type: 'daytime',
+                  boundary: 'end',
+                  time: new Date(),
+                }
+              ]
             }
           }
-        )
+        }
+
+        deferred.resolve(stub)
+
+        return deferred.promise
+
+        // APIがサービス終了してそうだったので、コメントアウト
+        // const today = new Date()
+        // return $http.jsonp('http://www.finds.jp/ws/movesun.php',
+        //   {
+        //     params: {
+        //       jsonp: 'JSON_CALLBACK',
+        //       lat: coords.latitude,
+        //       lon: coords.longitude,
+        //       y: today.getFullYear(),
+        //       m: today.getMonth() + 1,
+        //       d: today.getDate(),
+        //       tz: 9.0,
+        //       e: 0
+        //     }
+        //   }
+        // )
+        // }
       }
 
       getGeoPosition()
