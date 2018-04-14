115ce9630a7e27d1a8d6ffe2070318af54588d14
yinm <yinm.cminor9@gmail.com>
2018-04-14 16:42:08 +0900

    Check $cookiesProvider

--- chap05/other/scripts/cookies_def.js
+++ chap05/other/scripts/cookies_def.js
@@ -1,19 +1,19 @@
-angular.module('myApp', [ 'ngCookies' ])
-  .config(['$cookiesProvider', function($cookiesProvider) {
-     var expire = new Date();
-     expire.setMonth(expire.getMonth() + 6);
-     $cookiesProvider.defaults.expires = expire;
+angular.module('myApp', ['ngCookies'])
+  .config(['$cookiesProvider', ($cookiesProvider) => {
+    const expire = new Date()
+    expire.setMonth(expire.getMonth() + 6)
+    $cookiesProvider.defaults.expires = expire
   }])
 
-  .controller('MyController', ['$scope', '$cookies', function($scope, $cookies) {
-    $scope.email = $cookies.get('email');
-    $scope.record = true;
+  .controller('MyController', ['$scope', '$cookies', ($scope, $cookies) => {
+    $scope.email = $cookies.get('email')
+    $scope.record = true
 
-    $scope.onclick = function() {
-      if($scope.record) {
-        $cookies.put('email', $scope.email);
+    $scope.onclick = () => {
+      if ($scope.record) {
+        $cookies.put('email', $scope.email)
       } else {
-        $cookies.remove('email');
+        $cookies.remove('email')
       }
-    };
-  }]);
+    }
+  }])
