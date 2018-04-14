2d6942a19be3eaea93bc81087f321de97cccfa51
yinm <yinm.cminor9@gmail.com>
2018-04-14 10:56:53 +0900

    Check $locationProvider.html5Mode()

--- chap05/wrapper/scripts/location_html5.js
+++ chap05/wrapper/scripts/location_html5.js
@@ -1,11 +1,9 @@
 angular.module('myApp', [])
-  .config(function($locationProvider){
-     $locationProvider.html5Mode(true);
-  })
-  .controller('MyController',
-    ['$scope', '$location', function($scope, $location) {
-    $scope.onclick = function() {
-      $location.url('articles?id=108#wings');
-    };
-  }]);
-
+  .config(['$locationProvider', ($locationProvider) => {
+    $locationProvider.html5Mode(true)
+  }])
+  .controller('MyController', ['$scope', '$location', ($scope, $location) => {
+    $scope.onClick = () => {
+      $location.url('articles?id=108#wings')
+    }
+  }])
