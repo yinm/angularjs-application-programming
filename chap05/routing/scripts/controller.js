6429007be07c0e7e2f91f8cafffc2bd49dfad041
yinm <yinm.cminor9@gmail.com>
2018-04-14 09:45:00 +0900

    Check controllers and views

--- chap05/routing/scripts/controller.js
+++ chap05/routing/scripts/controller.js
@@ -1,41 +1,40 @@
 angular.module('myApp')
-  .controller('MainController', ['$scope', function($scope) {
-    $scope.msg = 'ようこそWINGSプロジェクトへ!';
+  .controller('MainController', ['$scope', ($scope) => {
+    $scope.msg = 'ようこそ'
   }])
 
-/*
-  .controller('MainController',
-  ['$scope', '$location', function($scope, $location) {
-    $scope.msg = 'ようこそWINGSプロジェクトへ!';
-    $scope.onclick = function() {
-      $location.path('/articles/13');
-    };
-  }])
-*/
+  /*
+    .controller('MainController',
+    ['$scope', '$location', function($scope, $location) {
+      $scope.msg = 'ようこそWINGSプロジェクトへ!';
+      $scope.onclick = function() {
+        $location.path('/articles/13');
+      };
+    }])
+  */
 
   .controller('ResolveController',
-  ['$scope', 'CurrentPosition', function($scope, CurrentPosition) {
-    $scope.pos = CurrentPosition;
+    ['$scope', 'CurrentPosition', function($scope, CurrentPosition) {
+      $scope.pos = CurrentPosition;
+    }])
+
+  .controller('ArticlesController', ['$scope', '$routeParams', ($scope, $routeParams) => {
+    $scope.id = $routeParams.id
   }])
 
+  // 第6章 ルーティング関連の$broadcastイベント
+  /*
+    .controller('ArticlesController',[
+      '$scope', '$routeParams', '$location', '$anchorScroll',
+      function($scope, $routeParams, $location, $anchorScroll) {
+        $scope.id = $routeParams.id;
+        $scope.$on('$routeChangeSuccess', function(e, new_r, old_r) {
+          $location.hash($routeParams.scroll);
+          $anchorScroll();
+        });
+    }])
+  */
 
-  .controller('ArticlesController', ['$scope', '$routeParams', function($scope, $routeParams) {
-    $scope.id = $routeParams.id;
+  .controller('SearchController', ['$scope', '$routeParams', ($scope, $routeParams) => {
+    $scope.keyword = $routeParams.keyword
   }])
-
-// 第6章 ルーティング関連の$broadcastイベント
-/*
-  .controller('ArticlesController',[
-    '$scope', '$routeParams', '$location', '$anchorScroll',
-    function($scope, $routeParams, $location, $anchorScroll) {
-      $scope.id = $routeParams.id;
-      $scope.$on('$routeChangeSuccess', function(e, new_r, old_r) {
-        $location.hash($routeParams.scroll);
-        $anchorScroll();  
-      });
-  }])
-*/
-
-  .controller('SearchController', ['$scope', '$routeParams', function ($scope, $routeParams) {
-    $scope.keyword = $routeParams.keyword;
-  }]);
