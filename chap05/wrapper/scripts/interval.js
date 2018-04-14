fac74ae338a6d58679124f6545b751d314e1a0d1
yinm <yinm.cminor9@gmail.com>
2018-04-14 10:18:57 +0900

    Check $interval

--- chap05/wrapper/scripts/interval.js
+++ chap05/wrapper/scripts/interval.js
@@ -1,11 +1,10 @@
 angular.module('myApp', [])
-  .controller('MyController',
-  ['$scope', '$interval', function($scope, $interval) {
-    var timer = $interval(function() {
-      $scope.current = new Date();
-    }, 1000);
+  .controller('MyController', ['$scope', '$interval', ($scope, $interval) => {
+    const timer = $interval(() => {
+      $scope.current = new Date()
+    }, 1000)
 
     $scope.onclick = function() {
       $interval.cancel(timer);
     };
-  }]);
+  }])
