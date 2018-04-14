21b7825778cc4f5400243c04f00ec388819d582d
yinm <yinm.cminor9@gmail.com>
2018-04-14 10:25:05 +0900

    Check $timeout service

--- chap05/wrapper/scripts/timeout.js
+++ chap05/wrapper/scripts/timeout.js
@@ -1,10 +1,10 @@
 angular.module('myApp', [])
-  .controller('MyController', ['$scope', '$timeout',  function($scope, $timeout) {
-    var timer = $timeout(function() {
-      $scope.greeting = 'ようこそ、世界！';
-    }, 10000);
+  .controller('MyController', ['$scope', '$timeout', ($scope, $timeout) => {
+    const timer = $timeout(() => {
+      $scope.greeting = 'Hello World'
+    }, 5000)
 
     $scope.onclick = function() {
       $timeout.cancel(timer);
     };
-  }]);
+  }])
