3cfaa7a1766bb9ef2610b45197680ae47361f7b8
yinm <yinm.cminor9@gmail.com>
2018-04-14 14:50:56 +0900

    Check then method

--- chap05/promise/scripts/geo.js
+++ chap05/promise/scripts/geo.js
@@ -1,26 +1,25 @@
 angular.module('myApp', [])
-  .controller('MyController',
-    ['$scope', '$http', '$q', '$window',
-    function($scope, $http, $q, $window) {
-    $scope.onclick = function() {
-      var getGeoPosition = function(success, error) {
-        var deferred = $q.defer();
+  .controller('MyController', ['$scope', '$http', '$q', '$window', ($scope, $http, $q, $window) => {
+    $scope.onclick = () => {
+      const getGeoPosition = (success, error) => {
+        const deferred = $q.defer()
         $window.navigator.geolocation.getCurrentPosition(
-          function(pos) {
-            return deferred.resolve(pos.coords);
+          (pos) => {
+            return deferred.resolve(pos.coords)
           },
-          function(err) {
-            return deferred.reject(err);
+          (err) => {
+            return deferred.reject(err)
           }
-        );
-        return deferred.promise;
-      };
+        )
 
-      var getSunset = function(coords) {
-        var today = new Date();
+        return deferred.promise
+      }
+
+      const getSunset = (coords) => {
+        const today = new Date()
         return $http.jsonp('http://www.finds.jp/ws/movesun.php',
           {
-            params : {
+            params: {
               jsonp: 'JSON_CALLBACK',
               lat: coords.latitude,
               lon: coords.longitude,
@@ -31,26 +30,26 @@ angular.module('myApp', [])
               e: 0
             }
           }
-        );
-      };
+        )
+      }
 
       getGeoPosition()
         .then(
-          function(coords) {
-            return getSunset(coords);
+          (coords) => {
+            return getSunset(coords)
           }
         )
         .then(
-          function(data) {
-            angular.forEach(data.data.result.event, function(value, index) {
+          (data) => {
+            angular.forEach(data.data.result.event, (value, index) => {
               if (value.type === 'daytime' && value.boundary === 'end') {
-                $scope.result = new Date(value.time).toLocaleString();
+                $scope.result = new Date(value.time).toLocaleString()
               }
-            });
+            })
           },
-          function(err) {
-            $window.alert(err.message);
+          (err) => {
+            $window.alert(err.message)
           }
-        );
-    };
-  }]);
+        )
+    }
+  }])
