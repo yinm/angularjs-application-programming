9a8d0bff125da919972277073fccbde3bb2affa0
yinm <yinm.cminor9@gmail.com>
2018-04-16 08:29:31 +0900

    Check $parent

---
+++ chap06/scripts/nest_proto2.js
@@ -0,0 +1,14 @@
+angular.module('myApp', [])
+  .controller('ParentController', ['$scope', ($scope) => {
+    $scope.value = 1
+
+    $scope.onparent = () => {
+      $scope.value++
+    }
+  }])
+
+  .controller('ChildController', ['$scope', ($scope) => {
+    $scope.onchild = () => {
+      $scope.$parent.value++
+    }
+  }])
