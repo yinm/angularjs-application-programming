85584fe4b6a7f1cb9c3b3c3f94528210ddeebe36
yinm <yinm.cminor9@gmail.com>
2018-04-16 08:37:09 +0900

    Check $root

--- chap06/scripts/scope_nest2.js
+++ chap06/scripts/scope_nest2.js
@@ -1,16 +1,14 @@
 angular.module('myApp', [])
-  .controller('ParentController', ['$scope', function($scope) {
-    $scope.$root.value = 1;
+  .controller('ParentController', ['$scope', ($scope) => {
+    $scope.$root.value = 1
 
-    $scope.onparent = function() {
-      $scope.$root.value++;
-    };
-
+    $scope.onparent = () => {
+      $scope.$root.value++
+    }
   }])
-  .controller('ChildController', ['$scope', function($scope) {
-    $scope.onchild = function() {
-      $scope.$root.value++;
-    };
-  }]);
 
-
+  .controller('ChildController', ['$scope', ($scope) => {
+    $scope.onchild = () => {
+      $scope.$root.value++
+    }
+  }])
