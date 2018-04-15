87597c0ebadc32c1f4a4496c371adb367db5f872
yinm <yinm.cminor9@gmail.com>
2018-04-16 08:26:48 +0900

    Check nest

--- chap06/scripts/nest_proto.js
+++ chap06/scripts/nest_proto.js
@@ -1,16 +1,14 @@
 angular.module('myApp', [])
-  .controller('ParentController', ['$scope', function($scope) {
-    $scope.value = 1;
+  .controller('ParentController', ['$scope', ($scope) => {
+    $scope.value = 1
 
-    $scope.onparent = function() {
-      $scope.value++;
-    };
-
+    $scope.onparent = () => {
+      $scope.value++
+    }
   }])
-  .controller('ChildController', ['$scope', function($scope) {
-    $scope.onchild = function() {
-      $scope.value++;
-      //$scope.$parent.value++;
-    };
-  }]);
 
+  .controller('ChildController', ['$scope', ($scope) => {
+    $scope.onchild = () => {
+      $scope.value++
+    }
+  }])
