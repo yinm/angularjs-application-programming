069132b4490e6b18fecf4e3cd3ac2320803007eb
yinm <yinm.cminor9@gmail.com>
2018-04-16 21:01:58 +0900

    Check $scope.$watch

--- chap06/scripts/watch.js
+++ chap06/scripts/watch.js
@@ -1,29 +1,10 @@
 angular.module('myApp', [])
-  .controller('MyController', ['$scope', function($scope) {
-    $scope.base = 0;
-    $scope.height = 0;
-    $scope.area = 0;
+  .controller('MyController', ['$scope', ($scope) => {
+    $scope.base = 0
+    $scope.height = 0
+    $scope.area = 0
 
-
-    $scope.$watch('base * height', function(newValue, oldValue, scope) {
-      //scope.height = scope.height + 1;
-      scope.area = scope.base * scope.height / 2;
-    });
-
-
-    /*
-    $scope.$watch('base * height / 2', function(newValue, oldValue, scope) {
-      scope.area = newValue;
-    });
-    */
-
-    /*
-    $scope.$watch(
-      function() {
-        return $scope.base * $scope.height / 2;
-      }, 
-      function(newValue, oldValue, scope) {
-        scope.area = newValue;
-      });
-    */
-  }]);
+    $scope.$watch('base * height', (newValue, oldValue, scope) => {
+      scope.area = scope.base * scope.height / 2
+    })
+  }])
