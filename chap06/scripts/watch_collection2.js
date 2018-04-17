d8aa2ea9880474ba12d6a1c58411bf8bbbb262a2
yinm <yinm.cminor9@gmail.com>
2018-04-17 09:21:04 +0900

    Check $scope.$watchCollection with object

--- chap06/scripts/watch_collection2.js
+++ chap06/scripts/watch_collection2.js
@@ -1,22 +1,22 @@
 angular.module('myApp', [])
-  .controller('MyController', ['$scope', function($scope) {
+  .controller('MyController', ['$scope', ($scope) => {
     $scope.book = {
       isbn: '978-4-7741-7078-7',
       title: 'サーブレット＆JSPポケットリファレンス',
       price: 2680,
       publish: '技術評論社',
       published: new Date(2015, 0, 8),
-      images : [ 'cover.jpg', 'logo.jpg' ]
-    };
+      images : [ 'cover.jpg', 'logo.jpg' ],
+    }
 
-    $scope.$watchCollection('book', function(newValue, oldValue, scope) {
-      console.log(newValue);
-    });
+    $scope.$watchCollection('book', (newValue, oldValue, scope) => {
+      console.log(newValue)
+    })
 
-    $scope.onclick = function() {
-      //$scope.book = {};
-      //$scope.book.title = '未定';
-      //$scope.book.author = '山田太郎';
-      $scope.book.images.push('cover2.jpg');
-    };
-  }]);
+    $scope.onclick = () => {
+      // $scope.book = {}
+      // $scope.book.title = 'foobar'
+      // $scope.book.author = '山田太郎'
+      $scope.book.images.push('cover2.jpg')
+    }
+  }])
