d392304711bf73d5c01d6105ad864ecb9741143d
yinm <yinm.cminor9@gmail.com>
2018-04-18 19:11:03 +0900

    Check $sce

--- chap03/bind/scripts/bind_html_trust.js
+++ chap03/bind/scripts/bind_html_trust.js
@@ -1,10 +1,9 @@
 angular.module('myApp', [])
-  .controller('MyController', ['$scope', '$sce', function($scope, $sce) {
-    var memo = '<p onmouseover="alert(\'OK!\')">ようこそ</p>'
-                  + '<a href="http://www.wings.msn.to">WINGSへ</a>'
-                  + '<script>var x = 1;</script>'
-                  + '<button>応募</button>';
-    $scope.memo = $sce.trustAsHtml(memo);
-    //$scope.memo  = $sce.trustAs($sce.HTML, memo);
+  .controller('MyController', ['$scope', '$sce', ($scope, $sce) => {
+    const memo = '<p onmouseover="alert(\'OK!\')">ようこそ</p>'
+      + '<a href="http://www.wings.msn.to">WINGSへ</a>'
+      + '<script>var x = 1;</script>'
+      + '<button>応募</button>';
 
-  }]);
+    $scope.memo = $sce.trustAsHtml(memo)
+  }])
