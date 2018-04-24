da2f64d3dda92f77da838ffc8acdf742a0c7ebf5
yinm <yinm.cminor9@gmail.com>
2018-04-24 14:03:46 +0900

    Check decorator()

--- chap07/service/scripts/decorator.js
+++ chap07/service/scripts/decorator.js
@@ -1,14 +1,13 @@
 angular.module('myApp', [])
-  .config(['$provide', function($provide) {
-    $provide.decorator('$log', ['$delegate', '$window',
-      function($delegate, $window) {
-      $delegate.error = function(message) {
-        $window.alert(message);
-      };
-      return $delegate;
-    }]);
+  .config(['$provide', ($provide) => {
+    $provide.decorator('$log', ['$delegate', '$window', ($delegate, $window) => {
+      $delegate.error = (message) => {
+        $window.alert(message)
+      }
+      return $delegate
+    }])
   }])
-  .controller('MyController', ['$scope', '$log', function($scope, $log) {
-    $log.error('エラー');
-    $log.warn('警告');
-  }]);
+  .controller('MyController', ['$scope', '$log', ($scope, $log) => {
+    $log.error('error')
+    $log.warn('warn')
+  }])
