11b3bbbff84ee40e67c36237393d5441e67eb96c
yinm <yinm.cminor9@gmail.com>
2018-04-24 09:45:38 +0900

    Check custom filter

--- chap07/filter/scripts/basic.js
+++ chap07/filter/scripts/basic.js
@@ -1,9 +1,11 @@
+// 2nd
 angular.module('myApp', ['ngSanitize'])
   .filter('nl2br', () => {
     return (value) => {
       if (!angular.isString(value)) {
         return value
       }
+
       return value.replace(/\r?\n/g, '<br>')
     }
   })
