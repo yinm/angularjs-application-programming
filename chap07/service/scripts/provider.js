eb2071b861de68767c1d030d92edf170f330186a
yinm <yinm.cminor9@gmail.com>
2018-04-24 13:44:33 +0900

    Check provider()

--- chap07/service/scripts/provider.js
+++ chap07/service/scripts/provider.js
@@ -1,30 +1,30 @@
 angular.module('myApp', [])
   .provider('MyLog', function() {
-    this.defaults = { logLevel : 4 };
+    this.defaults = { logLevel: 4 }
     this.$get = ['$log', function($log) {
-      var level = this.defaults.logLevel;
+      const level = this.defaults.logLevel
       return {
-        error : function(message) {
-          if (level > 0) { $log.error(message); }
+        error(message) {
+          if (level > 0) { $log.error(message) }
         },
-        warn : function(message) {
-          if (level > 1) { $log.warn(message); }
+        warn(message) {
+          if (level > 1) { $log.warn(message) }
         },
-        info : function(message) {
-          if (level > 2) { $log.info(message); }
+        info(message) {
+          if (level > 2) { $log.info(message) }
         },
-        debug : function(message) {
-          if (level > 3) { $log.debug(message); }
+        debug(message) {
+          if (level > 3) { $log.debug(message) }
         }
-      };
-    }];
+      }
+    }]
   })
-  .config(['MyLogProvider', function(MyLogProvider) {
-    MyLogProvider.defaults.logLevel = 3;
+  .config(['MyLogProvider', (MyLogProvider) => {
+    MyLogProvider.defaults.logLevel = 3
   }])
-  .controller('MyController', ['$scope', 'MyLog', function($scope, MyLog) {
-    MyLog.error('エラー');
-    MyLog.warn('警告');
-    MyLog.info('情報');
-    MyLog.debug('デバッグ');
-  }]);
+  .controller('MyController', ['$scope', 'MyLog', ($scope, MyLog) => {
+    MyLog.error('error')
+    MyLog.warn('warning')
+    MyLog.info('information')
+    MyLog.debug('debug')
+  }])
