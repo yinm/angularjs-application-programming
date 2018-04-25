821f2195b29bf126ee4c9540c82713bbd03c2d2a
yinm <yinm.cminor9@gmail.com>
2018-04-25 19:29:03 +0900

    Check isolated scope

--- chap07/directive/scripts/scope_isolate3.js
+++ chap07/directive/scripts/scope_isolate3.js
@@ -1,21 +1,18 @@
 angular.module('myApp', [])
-  .directive('myHello', function() {
+  .directive('myHello', () => {
     return {
       restrict: 'E',
-      replace: true,
-      transclude: true,
       scope: {
         type: '=',
         name: '@',
         click: '&'
       },
-      template: '<div ng-click="click()">{{type}}、{{name}}さん！</div>'
+      template: '<div ng-click="click()">{{type}}、{{name}}さん!</div>'
     }
   })
-  .controller('MyController', ['$scope', '$window', function($scope, $window) {
-    $scope.greeting = 'おはよう',
-    $scope.name = '山田',
-    $scope.onclick = function() {
-      $window.alert('クリックされました！');
+  .controller('MyController', ['$scope', '$window', ($scope, $window) => {
+    $scope.greeting = 'おはよう'
+    $scope.onclick = () => {
+      $window.alert('clicked!')
     }
-  }]);
+  }])
