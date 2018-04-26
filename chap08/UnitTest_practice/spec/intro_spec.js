039e18a495d8fcbff108a8a6881f6b2effbc8d01
yinm <yinm.cminor9@gmail.com>
2018-04-26 09:25:46 +0900

    Check jasmine notation

--- chap08/UnitTest_practice/spec/intro_spec.js
+++ chap08/UnitTest_practice/spec/intro_spec.js
@@ -1,10 +1,8 @@
-describe('Jasmineの基本', function() {
-  beforeEach(function(){
-  });
+describe('Jasmineの基本', () => {
+  beforeEach(() => {
+  })
 
-  it('基本テスト', function() {
-    expect(1 + 1).toEqual(2);
-    //expect(1 + 1).not.toEqual(2);
-    //expect(1 + 1).toEqual(3);
-  });
-});
+  it('基本テスト', () => {
+    expect(1 + 1).toEqual(2)
+  })
+})
