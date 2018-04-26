d5678605383996255fe0b854c6a6eb69a778714a
yinm <yinm.cminor9@gmail.com>
2018-04-26 10:22:28 +0900

    Check directive spec

--- chap08/UnitTest_practice/spec/directive_spec.js
+++ chap08/UnitTest_practice/spec/directive_spec.js
@@ -1,36 +1,39 @@
-describe('myBookディレクティブのテスト', function() {
-  var $scope, element, link;
+describe('myBook', () => {
+  let
+    $scope,
+    element,
+    link
 
-  beforeEach(module('myApp.directive'));
+  beforeEach(module('myApp.directive'))
 
   beforeEach(inject(function(_$compile_, _$rootScope_) {
-    var $compile = _$compile_;
-    var $rootScope = _$rootScope_;
+    const $compile = _$compile_
+    const $rootScope = _$rootScope_
 
-    element = angular.element('<my-book my-book="data"></my-book>');
-    link = $compile(element);
-    $scope = $rootScope.$new(true);
-    link($scope);
-  }));
+    element = angular.element('<my-book my-book="data"></my-book>')
+    link = $compile(element)
+    $scope = $rootScope.$new(true)
+    link($scope)
+  }))
 
-  it('ディレクティブの結果を確認する', function() {
-    var result = 'JavaScript本格入門（2,980円／技術評論社）';
-    var result2 = 'サーブレット＆JSPポケットリファレンス（2,680円／技術評論社）';
+  it('Check directive', () => {
+    const result = 'JavaScript本格入門（2,980円／技術評論社）'
+    const result2 = 'サーブレット＆JSPポケットリファレンス（2,680円／技術評論社）'
 
     $scope.data = {
       title: 'JavaScript本格入門',
       price: 2980,
       publish: '技術評論社',
-    };
-    $scope.$digest();
-    expect(element.text()).toEqual(result);
+    }
+    $scope.$digest()
+    expect(element.text()).toEqual(result)
 
     $scope.data = {
       title: 'サーブレット＆JSPポケットリファレンス',
       price: 2680,
       publish: '技術評論社',
-    };
-    $scope.$digest();
-    expect(element.text()).toEqual(result2);
-  });
-});
+    }
+    $scope.$digest()
+    expect(element.text()).toEqual(result2)
+  })
+})
